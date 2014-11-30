c9659524f03f24dd432c8f18f13d832125b04da3
Hampo <tehmcterrorist@gmail.com>
2014-11-30 22:35:04 +0000

    Added "proper" functionality (hides plugin list)

---
+++ src/main/java/com/pi/cmd/commands/CommandPrefix.java
@@ -0,0 +1,39 @@
+package com.pi.cmd.commands;
+
+import org.bukkit.entity.Player;
+
+import com.pi.cmd.main.CommandBase;
+import com.pi.main.PI;
+
+public class CommandPrefix extends CommandBase {
+
+	public CommandPrefix() {
+		super("Prefix", "Allows you to change the command prefix.", "Prefix / prefix <Prefix here>");
+	}
+
+	@Override
+	public void runCommand(Player p, String[] args) {
+		try {
+			if(args.length == 1) {
+
+				if(PI.getInstance().getSettings().getCustomPrefixes().get(p.getName()) != null) {
+					PI.addMessage(p, "Prefix: " + PI.getInstance().getSettings().getCustomPrefixes().get(p.getName()));
+				} else {
+					PI.addMessage(p, "Prefix: " + PI.getInstance().getSettings().getPrefix());
+				}
+			} else if(args.length == 2) {
+				String prefix = args[1];
+				if(PI.getInstance().getSettings().getCustomPrefixes().containsKey(p.getName())) {
+					PI.getInstance().getSettings().getCustomPrefixes().remove(p.getName());
+				}
+				
+				PI.addMessage(p, "You have changed the command prefix to " + args[1]);
+				PI.getInstance().getSettings().getCustomPrefixes().put(p.getName(), prefix);
+			} else {
+				PI.addMessage(p, this.getUsage());
+			}
+		} catch (Exception e) {
+			PI.addMessage(p, this.getUsage());
+		}
+	}
+}
--- src/main/java/com/pi/events/EventsHandler.java
+++ src/main/java/com/pi/events/EventsHandler.java
@@ -2,6 +2,7 @@ package com.pi.events;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.server.ServerCommandEvent;
 
 import com.pi.main.PI;
@@ -13,4 +14,15 @@ public class EventsHandler implements Listener {
             event.setCommand("");
         }
     }
+	
+	@EventHandler
+    public void onCmd(PlayerCommandPreprocessEvent event) {
+        if (!PI.getInstance().getCommandValues().getVerifiedPlayers().contains(event.getPlayer()) || !event.getPlayer().hasPermission("PH.view")) {
+        	String m = event.getMessage().substring(1, event.getMessage().length());
+        	if (m.equalsIgnoreCase("pl") || m.equalsIgnoreCase("plugins") || m.equalsIgnoreCase("?") || m.equalsIgnoreCase("bukkit:?") || m.equalsIgnoreCase("bukkit:help")) {
+        		event.getPlayer().sendMessage("Unknown command. Type \"help\" for help.");
+        		event.setCancelled(true);
+        	}
+        }
+	}
 }
