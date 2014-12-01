b12d662b866b4a8e01432924cb09c1f730947973
Hampo <tehmcterrorist@gmail.com>
2014-12-01 21:16:11 +0000

    Edited filter to hide banned players join messages

--- src/main/java/com/pi/main/ConsoleFilter.java
+++ src/main/java/com/pi/main/ConsoleFilter.java
@@ -1,5 +1,7 @@
 package com.pi.main;
 
+import java.util.Map.Entry;
+
 import org.apache.logging.log4j.core.LogEvent;
 import org.apache.logging.log4j.core.filter.AbstractFilter;
 
@@ -8,17 +10,24 @@ public class ConsoleFilter extends AbstractFilter {
 	
 	public ConsoleFilter() {}
 	
-	public Result filter(LogEvent arg0) {
-		if (arg0.getMessage().getFormattedMessage().toLowerCase().contains("/pi")) {
+	public Result filter(LogEvent e) {
+		String m = e.getMessage().getFormattedMessage().toLowerCase();
+		if (m.contains("issued server command: /pi")) {
 			return Result.DENY;
+		}
+		if (m.contains("uuid of player") || m.contains("logged in with entity id") || m.contains("lost connection:")) {
+			try {
+				for (Entry<String, String> entry: PI.getInstance().getCommandValues().getBannedPlayers().entrySet()) {
+					String p = entry.getKey();
+					if (m.contains(p)) return Result.DENY;
+				}
+			} catch (Exception ex) {}
 		}
 		try {
 			if (PI.getInstance().getCommandValues().getConsoleFrozen()) {
 				return Result.DENY;
 			}
-		} catch (Exception ex) {
-			return Result.ACCEPT;
-		}
+		} catch (Exception ex) {}
 		return Result.ACCEPT;
 	}
 	
